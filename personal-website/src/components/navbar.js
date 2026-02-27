5c3147de309485ae16cc4cf4c0b5a5a7dbc62c85
Troy <buradtroy323@berkeley.edu>
2026-02-27 13:52:48 -0800

    navbar prog

--- personal-website/src/components/navbar.js
+++ personal-website/src/components/navbar.js
@@ -1,4 +1,5 @@
 import { Nav, Navbar, Container } from "react-bootstrap";
+import {useState} from "react";
 
 export const NavBar = () => {
   return (
@@ -6,13 +7,16 @@ export const NavBar = () => {
       <Container>
         <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav"> 
+        <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#about-me">About Me</Nav.Link>
             <Nav.Link href="#projects">Projects</Nav.Link>
             <Nav.Link href="#misc">Misc</Nav.Link>
           </Nav>
+          <button className="vvd" onClick={() => console.log("connect")}>
+            <span>Test</span>
+          </button>
         </Navbar.Collapse>
       </Container>
     </Navbar>
