0551b4c2f3e52975ca376b0bb9066dec42a51ab4
Troy <buradtroy323@berkeley.edu>
2026-02-27 13:28:58 -0800

    navbar added

---
+++ personal-website/src/components/navbar.js
@@ -0,0 +1,20 @@
+import { Nav, Navbar, Container } from "react-bootstrap";
+
+export const NavBar = () => {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav"> 
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#about-me">About Me</Nav.Link>
+            <Nav.Link href="#projects">Projects</Nav.Link>
+            <Nav.Link href="#misc">Misc</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
